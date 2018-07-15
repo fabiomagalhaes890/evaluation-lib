80fc7ebab082e639c1c4b876d39c1c9070eaf765
fabiomagalhaes890 <fabiomagalhaes890@gmail.com>
2018-07-15 13:09:04 -0300

    Iniciando projeto

---
+++ evaluation.lib/DAL/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using EvaluationLib.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaluationLib.DAL
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ evaluation.lib/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EvaluationLib.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
